2ce6fcc811d9086140f08ee61a622947b8de96cb
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-05-18 17:28:13 +0900

    use clap 3

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
+    let _ = App::new("echor")
+        .version("0.1.0")
+        .about("Echo with Rust")
+        .get_matches();
     println!("{:?}", std::env::args());
 }
